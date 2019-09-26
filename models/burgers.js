82fe61005fae2e61b82e4dcd2c4284412b2b3823
Carlos Aragon <carlos.s.aragon12@gmail.com>
2019-09-25 17:51:00 -0700

    updated initial files from todo setup to burgurs setup. added new tables to the burger.js but need to separate them into new files

--- models/burgers.js
+++ models/burgers.js
@@ -1,6 +1,6 @@
-module.exports = function(sequelize, DataTypes) {
-  var Burger = sequelize.define("Todo", {
-    text: {
+module.exports = function (sequelize, DataTypes) {
+  var Burger = sequelize.define("burgers", {
+    type: {
       type: DataTypes.STRING,
       // AllowNull is a flag that restricts a todo from being entered if it doesn't
       // have a text value
@@ -10,12 +10,58 @@ module.exports = function(sequelize, DataTypes) {
         len: [1, 140]
       }
     },
-    complete: {
+    cooked: {
       type: DataTypes.BOOLEAN,
       // defaultValue is a flag that defaults a new todos complete value to false if
       // it isn't supplied one
       defaultValue: false
     }
   });
-  return Todo;
+  var Ingredient = sequelize.define("ingredients", {
+    patties: {
+      type: DataTypes.Integer,
+      defaultValue: 1,
+    },
+    buns: {
+      type: DataTypes.Integer,
+      defaultValue: 1,
+    },
+    cheese: {
+      type: DataTypes.Integer,
+      defaultValue: 1,
+    },
+    chile: {
+      type: DataTypes.Integer,
+      defaultValue: 1,
+    },
+  });
+  var Inventory = sequelize.define("inventories", {
+    patties: {
+      type: DataTypes.Integer,
+      allowNull: false
+    },
+    buns: {
+      type: DataTypes.Integer,
+      allowNull: false
+    },
+    cheese: {
+      type: DataTypes.Integer,
+      allowNull: false
+    },
+    chile: {
+      type: DataTypes.Integer,
+      allowNull: false
+    },
+  });
+  var Finance = sequelize.define("finances", {
+    income: {
+      type: DataTypes.Integer,
+      allowNull: false
+    },
+    expense: {
+      type: DataTypes.Integer,
+      allowNull: false
+    },
+  });
+  return Burger, Ingredient, Inventory, Finance;
 };
--- public/js/view.js
+++ public/js/view.js
@@ -2,54 +2,54 @@ $(document).ready(function() {
   // Getting a reference to the input field where user adds a new todo
   var $newItemInput = $("input.new-item");
   // Our new todos will go inside the todoContainer
-  var $todoContainer = $(".todo-container");
+  var $burgerContainer = $(".burger-container");
   // Adding event listeners for deleting, editing, and adding todos
-  $(document).on("click", "button.delete", deleteTodo);
+  $(document).on("click", "button.delete", deleteBurger);
   $(document).on("click", "button.complete", toggleComplete);
-  $(document).on("click", ".todo-item", editTodo);
-  $(document).on("keyup", ".todo-item", finishEdit);
-  $(document).on("blur", ".todo-item", cancelEdit);
-  $(document).on("submit", "#todo-form", insertTodo);
+  $(document).on("click", ".burger-item", editBurger);
+  $(document).on("keyup", ".burger-item", finishEdit);
+  $(document).on("blur", ".burger-item", cancelEdit);
+  $(document).on("submit", "#burger-form", insertBurger);
 
   // Our initial todos array
-  var todos = [];
+  var Burgers = [];
 
   // Getting todos from database when page loads
-  getTodos();
+  getBurgers();
 
   // This function resets the todos displayed with new todos from the database
   function initializeRows() {
-    $todoContainer.empty();
+    $burgerContainer.empty();
     var rowsToAdd = [];
-    for (var i = 0; i < todos.length; i++) {
-      rowsToAdd.push(createNewRow(todos[i]));
+    for (var i = 0; i < burgers.length; i++) {
+      rowsToAdd.push(createNewRow(burgers[i]));
     }
-    $todoContainer.prepend(rowsToAdd);
+    $burgerContainer.prepend(rowsToAdd);
   }
 
   // This function grabs todos from the database and updates the view
-  function getTodos() {
-    $.get("/api/todos", function(data) {
-      todos = data;
+  function getBurgers() {
+    $.get("/api/burgers", function(data) {
+      burgers = data;
       initializeRows();
     });
   }
 
   // This function deletes a todo when the user clicks the delete button
-  function deleteTodo(event) {
+  function deleteBurger(event) {
     event.stopPropagation();
     var id = $(this).data("id");
     $.ajax({
       method: "DELETE",
-      url: "/api/todos/" + id
-    }).then(getTodos);
+      url: "/api/burgers/" + id
+    }).then(getBurgers);
   }
 
   // This function handles showing the input box for a user to edit a todo
-  function editTodo() {
-    var currentTodo = $(this).data("todo");
+  function editBurger() {
+    var currentBurger = $(this).data("burger");
     $(this).children().hide();
-    $(this).children("input.edit").val(currentTodo.text);
+    $(this).children("input.edit").val(currentBurger.text);
     $(this).children("input.edit").show();
     $(this).children("input.edit").focus();
   }
@@ -57,50 +57,50 @@ $(document).ready(function() {
   // Toggles complete status
   function toggleComplete(event) {
     event.stopPropagation();
-    var todo = $(this).parent().data("todo");
-    todo.complete = !todo.complete;
-    updateTodo(todo);
+    var burger = $(this).parent().data("burger");
+    burger.complete = !burger.complete;
+    updateBurger(burger);
   }
 
   // This function starts updating a todo in the database if a user hits the "Enter Key"
   // While in edit mode
   function finishEdit(event) {
-    var updatedTodo = $(this).data("todo");
+    var updatedBurger = $(this).data("burger");
     if (event.which === 13) {
-      updatedTodo.text = $(this).children("input").val().trim();
+      updatedBurger.text = $(this).children("input").val().trim();
       $(this).blur();
-      updateTodo(updatedTodo);
+      updateBurger(updatedBurger);
     }
   }
 
   // This function updates a todo in our database
-  function updateTodo(todo) {
+  function updateBurger(burger) {
     $.ajax({
       method: "PUT",
-      url: "/api/todos",
-      data: todo
-    }).then(getTodos);
+      url: "/api/burgers",
+      data: burger
+    }).then(getBurgers);
   }
 
   // This function is called whenever a todo item is in edit mode and loses focus
   // This cancels any edits being made
   function cancelEdit() {
-    var currentTodo = $(this).data("todo");
-    if (currentTodo) {
+    var currentBurger = $(this).data("burger");
+    if (currentBurger) {
       $(this).children().hide();
-      $(this).children("input.edit").val(currentTodo.text);
+      $(this).children("input.edit").val(currentBurger.text);
       $(this).children("span").show();
       $(this).children("button").show();
     }
   }
 
   // This function constructs a todo-item row
-  function createNewRow(todo) {
+  function createNewRow(burger) {
     var $newInputRow = $(
       [
-        "<li class='list-group-item todo-item'>",
+        "<li class='list-group-item burger-item'>",
         "<span>",
-        todo.text,
+        burger.text,
         "</span>",
         "<input type='text' class='edit' style='display: none;'>",
         "<button class='delete btn btn-danger'>x</button>",
@@ -109,24 +109,24 @@ $(document).ready(function() {
       ].join("")
     );
 
-    $newInputRow.find("button.delete").data("id", todo.id);
+    $newInputRow.find("button.delete").data("id", burger.id);
     $newInputRow.find("input.edit").css("display", "none");
-    $newInputRow.data("todo", todo);
-    if (todo.complete) {
+    $newInputRow.data("burger", burger);
+    if (burger.complete) {
       $newInputRow.find("span").css("text-decoration", "line-through");
     }
     return $newInputRow;
   }
 
   // This function inserts a new todo into our database and then updates the view
-  function insertTodo(event) {
+  function insertBurger(event) {
     event.preventDefault();
-    var todo = {
+    var burger = {
       text: $newItemInput.val().trim(),
       complete: false
     };
 
-    $.post("/api/todos", todo, getTodos);
+    $.post("/api/burgers", burger, getBurgers);
     $newItemInput.val("");
   }
 });
